4f53fae94584dd9acd2af298eeba72e03979dab5
sunga1 <114139033+sunga1@users.noreply.github.com>
2024-03-21 11:28:21 +0900

    Delete src/main/java/sungaron/foodiespot/oauth directory

--- src/main/java/sungaron/foodiespot/oauth/response/OAuthInfoResponse.java
+++
@@ -1,9 +0,0 @@
-package sungaron.foodiespot.oauth.response;
-
-import sungaron.foodiespot.oauth.OAuthProvider;
-
-public interface OAuthInfoResponse {
-    String getEmail();
-    String getNickname();
-    OAuthProvider getOAuthProvider();
-}
